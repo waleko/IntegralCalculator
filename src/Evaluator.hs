569e0d69e760a29fd46ab07f175b055c20631885
Alex Kovrigin <a.kovrigin0@gmail.com>
2022-11-20 23:26:06 +0000

    absolute error handling + rectangles

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -8,17 +8,31 @@ data Bounds = Bounds {
     upper :: Double
 }
 
-data Strategy = Linear | Trapezoid | Paraboloid
+data Strategy = Rectangle | Trapezoid | Paraboloid
 
 evalWithN :: Strategy -> Bounds -> Func -> Int -> Double
-evalWithN Linear (Bounds low up) f n = sum [(f(x) + f(x + delta)) / 2 | x <- xs] * delta
+evalWithN Rectangle (Bounds low up) f n = sum [f(x) | x <- xs] * delta
+    where
+        delta = (up - low) / fromIntegral (n)
+        xs = [low + delta * (0.5 + fromIntegral idx) | idx <- [0..(n - 1)]]
+-- TODO: improper + diverging
+evalWithN Trapezoid (Bounds low up) f n = sum [(f(x) + f(x + delta)) / 2 | x <- xs] * delta
     where
         delta = (up - low) / fromIntegral (n)
         xs = [low + delta * (fromIntegral idx) | idx <- [0..(n - 1)]]
 
-stoppingCriteria :: Double
-stoppingCriteria = 1e-8
+data IntegralProps = IntegralProps {
+    strategy :: Strategy,
+    maxError :: Double
+}
 
-eval :: Strategy -> Bounds -> Func -> Double
-eval strategy bounds func = evalHelper 0 1
-    where evalHelper prev n = let val = evalWithN strategy bounds func n in if abs(val - prev) < stoppingCriteria * abs(prev) then val else evalHelper val (n + 1)
+data IntegralResult = IntegralResult {
+    value :: Double,
+    diverging :: Bool,
+    steps :: Int,
+    error :: Double
+} deriving (Show, Eq)
+
+eval :: IntegralProps -> Bounds -> Func -> IntegralResult
+eval (IntegralProps strategy maxError) bounds func = evalHelper 0 1
+    where evalHelper prev n = let val = evalWithN strategy bounds func n in if abs(val - prev) < maxError then (IntegralResult val False (2 * n - 1) (abs (val - prev))) else evalHelper val (2 * n)
