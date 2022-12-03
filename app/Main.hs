d70f88d1f5642d14479608d2912045d0a2935a55
Alex Kovrigin <a.kovrigin0@gmail.com>
2022-12-03 22:40:02 +0000

    [WIP] parser work

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,55 @@
 module Main (main) where
 
--- import Lib
+import Integral
+import Options.Applicative
+import Text.Printf (printf)
 
 main :: IO ()
-main = undefined
+main = do
+  runAction =<< execParser opts
+  where
+    -- Задает парсер аргументов actionParser, сопровождая его автоматической генерацией странички help.
+    opts =
+      info
+        (actionParser <**> helper)
+        ( fullDesc
+            <> progDesc "This application demonstrates two parsers for arithmetic expressions: one for the infix and the other for the prefix notation."
+            <> header "Simple parser console application"
+        )
+
+-- Тип входных данных
+data Input
+  = StrInput String | FileInput String -- Строка, передаваемая аргументом командной строки
+  deriving (Show)
+
+actionParser :: Parser Action
+actionParser = Action <$> inputParser
+
+-- Парсер аргумента, специфицирующий, откуда брать входные данные
+inputParser :: Parser Input
+inputParser = fileInput <|> strInput
+
+-- Флаг -i/--input позволяет задать строку -- имя входного файла
+fileInput :: Parser Input
+fileInput =
+  FileInput
+    <$> strOption --
+      ( short 'i' -- короткое имя флага (-i)
+          <> long "input" -- длинное имя флага (--input)
+          <> metavar "INPUT" -- как аргумент этой опции называется в документации
+          <> help "Input file" --
+      )
+
+-- Можно не использовать флаг i, а просто написать входную строку (1+2 в stack run exe -- 1+2)
+strInput :: Parser Input
+strInput = StrInput <$> strArgument (metavar "STRING" <> help "String to be parsed")
+
+-- Тип данных, агрегирующий все аргументы командной строки, возвращается actionParser-ом
+data Action = Action {
+    input :: Input
+} deriving (Show)
+
+-- Основная функция приложения
+runAction :: Action -> IO ()
+runAction (Action inp) = do
+  putStrLn $ printf "aboba"
